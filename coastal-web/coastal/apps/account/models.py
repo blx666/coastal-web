35b195f27ec3b8df10b0d77f48eb43fdcfad6934
liuyijun <liuyijun@aragoncs.com>
2016-12-14 15:36:47 +0800

    update account & product models

--- coastal-web/coastal/apps/account/models.py
+++ coastal-web/coastal/apps/account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from coastal.apps.product.models import Product
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     is_agent = models.NullBooleanField()
@@ -24,4 +25,10 @@ class Favorites(models.Model):
 
 class FavoriteItem(models.Model):
     favorite = models.ForeignKey(Favorites, related_name='items')
-    product = models.OneToOneField(Product)
+    product = models.ForeignKey(Product)
+
+
+class RecentlyViewed(models.Model):
+    user = models.ForeignKey(User, related_name='recently_viewed')
+    product = models.ForeignKey(Product)
+    date_created = models.DateTimeField(auto_now_add=True)
