2ae79951a327c8009f32fc82c3bfe6411e25f889
= <=>
2016-12-15 15:25:53 +0800

    API - add coastal api response

--- coastal-web/coastal/api/account/views.py
+++ coastal-web/coastal/api/account/views.py
@@ -29,7 +29,7 @@ def login(request):
     if request.method != 'POST':
         return CoastalJsonResponse(status=response.STATUS_405)
 
-    user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+    user = authenticate(username=request.POST.get('user'), password=request.POST.get('password'))
     if user:
         auth_login(request, user)
         data = {
