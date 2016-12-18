b0c2e50358f4387eca54f69f14ee575839f38095
liuyijun <liuyijun@aragoncs.com>
2016-12-18 11:47:34 +0800

    update profile

--- coastal-web/coastal/api/account/forms.py
+++ coastal-web/coastal/api/account/forms.py
@@ -21,10 +21,17 @@ class RegistrationForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     first_name = forms.CharField(max_length=128, required=False)
     last_name = forms.CharField(max_length=128, required=False)
+    is_agent = forms.CharField(required=False)
+
+    def clean_is_agent(self):
+        value = self.cleaned_data['is_agent']
+        if value not in ('0', '1'):
+            raise forms.ValidationError("The value should be boolean: 0/1")
+        return value == '1'
 
     class Meta:
         model = UserProfile
-        fields = ['is_agent', 'agency_email', 'agency_name', 'agency_address', 'photo']
+        fields = ['agency_email', 'agency_name', 'agency_address', 'photo']
 
 
 class CheckEmailForm(forms.Form):
