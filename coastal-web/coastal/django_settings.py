0f21f96e6216d5a5dcd704cf1b61a90d4ab200cc
wangyufei911 <wangyufei@aragoncs.com>
2016-12-24 12:00:07 +0800

    update product api currency

--- coastal-web/coastal/django_settings.py
+++ coastal-web/coastal/django_settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'django_crontab',
+    # 'django_crontab',
     'coastal.apps.product',
     'coastal.apps.account',
     'coastal.apps.promotion',
