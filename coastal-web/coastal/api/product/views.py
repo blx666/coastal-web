84d3c11da338551e2e60b944cdf4fd9779b05128
mmmabiao <mabiao@aragoncs.com>
2016-12-29 20:24:46 +0800

    discount price

--- coastal-web/coastal/api/product/views.py
+++ coastal-web/coastal/api/product/views.py
@@ -302,7 +302,6 @@ def calc_price(product,start_date,end_date):
     if rental_unit == 'half-day':
         rental_price *= 4
     rental_date = (end_date - start_date).seconds / 3600 / 24 + (end_date - start_date).days
-    rental_amount = 0
     if rental_date >= 30:
         rental_amount = math.ceil(rental_date * rental_price * (1 - product.discount_monthly/100))
     elif rental_date >= 7:
