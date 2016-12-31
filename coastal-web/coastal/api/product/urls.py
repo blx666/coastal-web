c379e64adede534d28f7127a533a8c94e6168073
wangyufei911 <wangyufei@aragoncs.com>
2016-12-31 14:31:28 +0800

    update currency

--- coastal-web/coastal/api/product/urls.py
+++ coastal-web/coastal/api/product/urls.py
@@ -4,7 +4,7 @@ from coastal.api.product import views
 
 
 urlpatterns = [
-    url(r'^page/(?P<page>\d+)/$', views.product_list, name='product-list'),
+    url(r'^$', views.product_list, name='product-list'),
     url(r'^(?P<pid>\d+)/$', views.product_detail, name='product-detail'),
     url(r'^update/$', views.product_update, name='product-update'),
     url(r'^upload-image/$', views.product_image_upload, name='product-image-upload'),
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^amenities/$', views.amenity_list, name='amenity-list'),
     url(r'^(?P<pid>\d+)/like-toggle/$', views.toggle_favorite, name='product-like-toggle'),
     url(r'^currency-list/$', views.currency_list, name='currency-list'),
-    url(r'^recommend/(?P<page>\d+)/$', views.recommend_product_list, name='recommend-product-list'),
+    url(r'^recommend/$', views.recommend_product_list, name='recommend-product-list'),
     url(r'^discount-calculator/$', views.discount_calculator, name='discount-calculator'),
     url(r'^delete-image/$', views.delete_image, name='delete-image'),
     url(r'^(?P<pid>\d+)/black-dates-for-rental/$', views.black_dates_for_rental, name='black-dates-for-rental'),
