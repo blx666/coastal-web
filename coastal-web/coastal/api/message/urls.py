48f4a3c3c800dfa79de52bbda053a1f8c168de03
wangxudong <woshiwangxuwang@163.com>
2017-01-10 15:27:42 +0800

    修改

--- coastal-web/coastal/api/message/urls.py
+++ coastal-web/coastal/api/message/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     url(r'^list/$', views.dialogue_list, name='dialogue-list'),
     url(r'^view/$', views.dialogue_detail, name='dialogue-detail'),
     url(r'^send-message/$', views.send_message, name='send-message'),
-    #url(r'^get_new_message/$', views.get_new_message, name='get-new-message'),
 ]
