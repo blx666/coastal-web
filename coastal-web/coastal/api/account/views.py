7a93683379ae855dcaf09d01488c3928f24db579
= <=>
2017-01-13 17:57:34 +0800

    API - BUG my_calendar and my_account

--- coastal-web/coastal/api/account/views.py
+++ coastal-web/coastal/api/account/views.py
@@ -14,6 +14,7 @@ from coastal.api.core.decorators import login_required
 from coastal.apps.account.models import UserProfile, ValidateEmail, FavoriteItem
 from coastal.apps.product.models import Product
 from coastal.apps.rental.models import RentalOrder
+from coastal.apps.sale.models import SaleOffer
 from datetime import datetime, timedelta, time
 from coastal.apps.product import defines as defs
 from coastal.api.product.utils import bind_product_image
@@ -313,15 +314,23 @@ def my_account(request):
 
     # my orders
     order_group = []
-    order_list = user.owner_orders.all()
+    order_rental_list = list(RentalOrder.objects.filter(Q(owner=user) | Q(guest=user),
+                                                        status__in=['finished', 'declined', 'invalid'])
+                             .order_by('date_created'))
+    order_sale_list = list(SaleOffer.objects.filter(Q(owner=user) | Q(guest=user),
+                                                    status__in=['finished', 'declined', 'invalid'])
+                           .order_by('date_created'))
+    order_list = order_rental_list + order_sale_list
     for order in order_list:
-        data_order = {}
-        data_order['id'] = order.id
-        data_order['type'] = order.rental_unit
-        image = order.product.productimage_set.all()
-        data_order['image'] = image[0].image.url if len(image) else ''
-        data_order['title'] = order.number
-        order_group.append(data_order)
+        if order.date_updated + timedelta(days=1) < timezone.now():
+            data_order = {}
+            data_order['id'] = order.id
+            data_order['type'] = 'rental' if isinstance(order, RentalOrder) else 'sale'
+            image = order.product.productimage_set.all()
+            data_order['image'] = image[0].image.url if len(image) else ''
+            data_order['title'] = order.number
+            order_group.append(data_order)
+
     data['orders'] = order_group
 
     return CoastalJsonResponse(data)
@@ -329,11 +338,9 @@ def my_account(request):
 
 @login_required
 def my_calendar(request):
-    if request.method != 'POST':
-        return CoastalJsonResponse(status=response.STATUS_405)
-
     user = request.user
-    month = time.strptime(request.POST.get('month'), '%Y-%m')
+
+    month = time.strptime(request.GET.get('month'), '%Y-%m')
     order_list = user.owner_orders.all()
     data_result = []
     orders = {}
