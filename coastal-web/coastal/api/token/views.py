430ff0e8beda59e7d08f798fe440461e70cd9dfe
wangxudong <woshiwangxuwang@163.com>
2017-01-13 19:08:07 +0800

    修改

--- coastal-web/coastal/api/token/views.py
+++ coastal-web/coastal/api/token/views.py
@@ -3,7 +3,7 @@ import boto3
 from coastal.api.core import response
 from coastal.api.core.response import CoastalJsonResponse
 from django.conf import settings
-from coastal.apps.sign.models import Token
+from coastal.apps.sns.models import Token
 
 
 def bind_token(request):
@@ -15,21 +15,21 @@ def bind_token(request):
     aws_key = settings.AWS_ACCESS_KEY_ID
     aws_secret = settings.AWS_SECRET_ACCESS_KEY
     region_name = settings.REGION
-    session = Session(aws_access_key_id=aws_key, aws_secret_access_key=aws_secret, region_name=region_name)
-    rds = boto3.setup_default_session(aws_access_key_id=aws_key, aws_secret_access_key=aws_secret, region_name=region_name)
-    rds = boto3.client('sns')
+    Session(aws_access_key_id=aws_key, aws_secret_access_key=aws_secret, region_name=region_name)
+    boto3.setup_default_session(aws_access_key_id=aws_key, aws_secret_access_key=aws_secret, region_name=region_name)
+    aws = boto3.client('sns')
 
     token_obj = Token.objects.filter(uuid=uuid, token=token).first()
     if not token_obj:
-        endpoint = rds.create_platform_endpoint(
+        endpoint = aws.create_platform_endpoint(
             PlatformApplicationArn=settings.PLATFORM_APPLICATION_ARN,
             Token=token
         )
         endpoint_arn = endpoint['EndpointArn']
-        Token.objects.update_or_create(uuid=uuid, defaults={'user': request.user, 'token': token,
-                                                            'endpoint': endpoint_arn, 'uuid': uuid})
     else:
         endpoint_arn = token_obj.endpoint
+    Token.objects.update_or_create(uuid=uuid, defaults={'user': request.user, 'token': token,
+                                                        'endpoint': endpoint_arn, 'uuid': uuid})
 
     result = {
         'endpoint_arn': endpoint_arn,
