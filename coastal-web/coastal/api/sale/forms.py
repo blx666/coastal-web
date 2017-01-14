b0fd107b2a482cce22822eb4ab2a0c120347b9c7
wangyufei911 <wangyufei@aragoncs.com>
2017-01-14 15:04:01 +0800

    Merge branch 'api-develop' of gitlab.com:aragoncs/coastal into api-develop
    # Conflicts:
    #	coastal-web/coastal/api/sale/urls.py
    #	coastal-web/coastal/api/sale/views.py

--- coastal-web/coastal/api/sale/forms.py
+++ coastal-web/coastal/api/sale/forms.py
@@ -13,3 +13,14 @@ class SaleOfferForm(forms.ModelForm):
         product = self.cleaned_data.get('product')
         if not product.for_sale:
                 raise forms.ValidationError('the product cannot be sold.')
+
+
+class SaleApproveForm(forms.Form):
+    approve = forms.CharField()
+    note = forms.CharField(required=False)
+
+    def clean_approve(self):
+        value = self.cleaned_data.get('approve')
+        if value not in ('0', '1'):
+            raise forms.ValidationError("The value should be boolean: 0/1")
+        return value == '1'
