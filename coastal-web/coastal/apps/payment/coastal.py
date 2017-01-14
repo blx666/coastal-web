c6e4b600bc771b664bb650736c30dea32d091869
liuyijun <liuyijun@aragoncs.com>
2017-01-14 15:57:38 +0800

    payment

--- coastal-web/coastal/apps/payment/coastal.py
+++ coastal-web/coastal/apps/payment/coastal.py
@@ -3,7 +3,7 @@ from coastal.apps.rental.models import PaymentEvent
 
 
 def charge(rental_order, user):
-    if user.coastalbucket.balance < rental_order.total_amount_usd:
+    if user.coastalbucket.balance < rental_order.total_price_usd:
         return False
 
     Transaction.objects.create(
@@ -12,17 +12,17 @@ def charge(rental_order, user):
         order_number=rental_order.number
     )
 
-    user.coastalbucket.balance -= rental_order.total_amount_usd
+    user.coastalbucket.balance -= rental_order.total_price_usd
     user.coastalbucket.save()
 
     PaymentEvent.objects.create(
         order=rental_order,
         payment_type='coastal',
-        amount=rental_order.total_amount_usd,
+        amount=rental_order.total_price_usd,
         currency='USD',
     )
 
-    rental_order.coastal_dollar = rental_order.total_amount_usd
+    rental_order.coastal_dollar = rental_order.total_price_usd
     rental_order.status = 'booked'
     rental_order.save()
 
