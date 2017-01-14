e5a1f4e7ddd4ebfa4e854f62b712b3b505ccb424
liuyijun <liuyijun@aragoncs.com>
2017-01-14 18:41:21 +0800

    add celery for order process

---
+++ coastal-web/coastal/apps/rental/tasks.py
@@ -0,0 +1,79 @@
+from celery import shared_task
+from coastal.apps.rental.models import RentalOrder
+
+
+@shared_task
+def expire_order_request(order_id):
+    try:
+        order = RentalOrder.objects.get(id=order_id)
+    except RentalOrder.DoesNotExist:
+        # TODO: add log
+        return
+
+    if order.status == 'request':
+        order.status = 'invalid'
+        order.save()
+
+        # TODO: send notification
+
+
+@shared_task
+def expire_order_charge(order_id):
+    try:
+        order = RentalOrder.objects.get(id=order_id)
+    except RentalOrder.DoesNotExist:
+        # TODO: add log
+        return
+
+    if order.status == 'charge':
+        order.status = 'invalid'
+        order.save()
+
+        # TODO: send notification
+
+
+@shared_task
+def check_in(order_id):
+    try:
+        order = RentalOrder.objects.get(id=order_id)
+    except RentalOrder.DoesNotExist:
+        # TODO: add log
+        return
+
+    if order.status == 'booked':
+        order.status = 'check_in'
+        order.save()
+
+        pay_owner.apply_async((order_id,), countdown=3 * 60 * 60)
+
+
+@shared_task
+def pay_owner(order_id):
+    try:
+        order = RentalOrder.objects.get(id=order_id)
+    except RentalOrder.DoesNotExist:
+        # TODO: add log
+        return
+
+    if order.status == 'check-in':
+        # TODO: pay owner coastal dollar
+
+        order.status = 'paid'
+        order.save()
+
+        # TODO: send notification
+
+
+@shared_task
+def check_out(order_id):
+    try:
+        order = RentalOrder.objects.get(id=order_id)
+    except RentalOrder.DoesNotExist:
+        # TODO: add log
+        return
+
+    if order.status == 'paid':
+        order.status = 'finished'
+        order.save()
+
+        # TODO: send notification
