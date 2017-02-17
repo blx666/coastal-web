dc3f9d9cc62c2382b25e7301a5688f5b06ad03f8
mmmabiao <mabiao@aragoncs.com>
2017-02-17 09:23:28 +0800

    product

--- coastal-web/coastal/apps/rental/utils.py
+++ coastal-web/coastal/apps/rental/utils.py
@@ -20,44 +20,44 @@ def rental_out_date(product, start_datetime, end_datetime):
     if product.category_id == defs.CATEGORY_EXPERIENCE:
         if product.exp_time_unit != 'hour':
             if product.exp_time_unit == 'day':
-                start_datetime -= datetime.timedelta(days=product.exp_time_length)
-                end_datetime += datetime.timedelta(days=product.exp_time_length)
+                start_range = start_datetime - datetime.timedelta(days=product.exp_time_length)
+                end_range = end_datetime + datetime.timedelta(days=product.exp_time_length)
             else:
-                start_datetime -= datetime.timedelta(days=product.exp_time_length * 7)
-                end_datetime += datetime.timedelta(days=product.exp_time_length * 7)
-            start_out_date = RentalOutDate.objects.filter(end_date__gte=start_datetime, end_date__lte=end_datetime, product=product)
-            end_out_date = RentalOutDate.objects.filter(start_date__lte=start_datetime, start_date__gte=end_datetime, product=product)
+                start_range = start_datetime -datetime.timedelta(days=product.exp_time_length * 7)
+                end_range = end_datetime + datetime.timedelta(days=product.exp_time_length * 7)
+            start_out_date = RentalOutDate.objects.filter(end_date__gte=start_range, end_date__lte=end_range, product=product)
+            end_out_date = RentalOutDate.objects.filter(start_date__lte=start_range, start_date__gte=end_range, product=product)
             if start_out_date and end_out_date:
                 start_out_date.update(end_date=end_out_date[0].end_date)
                 end_out_date.delete()
             elif start_out_date:
-                start_out_date.update(end_date=end_datetime)
+                start_out_date.update(end_date=end_range)
             elif end_out_date:
-                end_out_date.update(start_date=start_datetime)
+                end_out_date.update(start_date=start_range)
             else:
                 RentalOutDate.objects.create(product=product, start_date=start_datetime, end_date=end_datetime)
         else:
             today_begin = start_datetime.replace(hour=0, minute=0, second=0)
             today_end = today_begin.replace(hour=23, minute=59, second=59)
-            if start_datetime- datetime.timedelta(hours=product.exp_time_length) < today_begin:
-                start_datetime = today_begin
+            if start_datetime - datetime.timedelta(hours=product.exp_time_length) < today_begin:
+                start_range = today_begin
             else:
-                start_datetime -= datetime.timedelta(hours=product.exp_time_length)
+                start_range = start_datetime - datetime.timedelta(hours=product.exp_time_length)
             if end_datetime + datetime.timedelta(hours=product.exp_time_length) > today_end:
-                end_datetime = today_end
+                end_range = today_end
             else:
-                end_datetime += datetime.timedelta(hours=product.exp_time_length)
+                end_range = end_datetime + datetime.timedelta(hours=product.exp_time_length)
             rental_date = RentalOutDate.objects.filter(product=product, start_date__gte=today_begin,
                                                        end_date__lte=today_end)
-            start_out_date = rental_date.filter(end_date__gte=start_datetime, end_date__lte=end_datetime)
-            end_out_date = rental_date.filter(start_date__lte=start_datetime, start_date__gte=end_datetime)
+            start_out_date = rental_date.filter(end_date__gte=start_range, end_date__lte=end_datetime)
+            end_out_date = rental_date.filter(start_date__lte=start_datetime, start_date__gte=end_range)
             if start_out_date and end_out_date:
                 start_out_date.update(end_date=end_out_date[0].end_date)
                 end_out_date.delete()
             elif start_out_date:
-                start_out_date.update(end_date=end_datetime)
+                start_out_date.update(end_date=end_range)
             elif end_out_date:
-                end_out_date.update(start_date=start_datetime)
+                end_out_date.update(start_date=start_range)
             else:
                 RentalOutDate.objects.create(product=product, start_date=start_datetime, end_date=end_datetime)
             product_start = today_begin.replace(hour=product.exp_start_time.hour,minute=product.exp_start_time.minute)
