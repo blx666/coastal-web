95d2d5c5c787c3d66fe2a9ae16ccd291dfd4376e
wangyufei911 <wangyufei@aragoncs.com>
2017-02-20 19:59:54 +0800

    sign in

--- coastal-web/coastal/django_settings.py
+++ coastal-web/coastal/django_settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'coastal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'coastal/templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
