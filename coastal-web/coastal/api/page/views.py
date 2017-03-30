19161e419854f1a4263ddb90c633f8e91201bd25
liuyijun <liuyijun@aragoncs.com>
2017-03-30 17:00:45 +0800

    UPD: #706 add cache for home page

--- coastal-web/coastal/api/page/views.py
+++ coastal-web/coastal/api/page/views.py
@@ -1,7 +1,8 @@
 from django.forms.models import model_to_dict
+from django.core.cache import cache
 from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
-from django.core.paginator import PageNotAnInteger
+from django.db.models import Avg, Count
 from django.views.decorators.cache import cache_page
 
 from coastal.api.core.response import CoastalJsonResponse
@@ -11,17 +12,42 @@ from coastal.api.product.utils import bind_product_main_image
 from coastal.apps.account.models import FavoriteItem
 from coastal.api import defines as defs
 from coastal.apps.product import defines as product_defs
-from coastal.apps.currency.utils import price_display
-from django.db.models import Avg, Count
 from coastal.apps.review.models import Review
+from coastal.core import cache_defines as cache_keys
 
 
 def home(request):
-    page = request.GET.get('page', '1')
-    # get home_banner
-    home_banners = HomeBanner.objects.order_by('display_order')
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    product_list, next_page = get_home_product_list(page)
+
+    liked_product_id_list = []
+    if request.user.is_authenticated:
+        liked_product_id_list = FavoriteItem.objects.filter(favorite__user=request.user).values_list(
+            'product_id', flat=True)
+    for p in product_list:
+        p['liked'] = p['id'] in liked_product_id_list
+
+    result = {
+        'home_banner': get_home_banners(),
+        'products': product_list,
+        'next_page': next_page
+    }
+    return CoastalJsonResponse(result)
+
+
+def get_home_banners():
+    home_banners_list = cache.get(cache_keys.CACHE_HOME_BANNER_KEY)
+
+    if home_banners_list is not None:
+        return home_banners_list
+
     home_banners_list = []
-    for banner in home_banners:
+
+    for banner in HomeBanner.objects.order_by('display_order'):
         banner_dict = {
             'city_name': banner.city_name,
             'image': banner.image.url,
@@ -41,38 +67,51 @@ def home(request):
 
         home_banners_list.append(banner_dict)
 
-    # get recommended products
-    products = Product.objects.filter(status='published').order_by('-rank', '-score', '-rental_usd_price', '-sale_usd_price')
-    space_products = products.filter(category__in=product_defs.SPACE_CATEGORY_LIST)
-    yacht_products = products.filter(category__in=product_defs.YACHT_CATEGORY_LIST)
-    adventure_products = products.filter(category=product_defs.CATEGORY_ADVENTURE)
+    cache.set(cache_keys.CACHE_HOME_BANNER_KEY, home_banners_list, 5 * 60)
+
+    return home_banners_list
+
+
+def get_home_product_list(page):
+    """
+    :param page: int
+    :return:
+    """
+    home_product_list_result = cache.get(cache_keys.CACHE_HOME_PRODUCT_LIST_KEY % page)
+
+    if home_product_list_result is not None:
+        return home_product_list_result
+
+    published_product_query = Product.objects.filter(status='published').order_by(
+        '-rank', '-score', '-rental_usd_price', '-sale_usd_price')
+    space_products = published_product_query.filter(category__in=product_defs.SPACE_CATEGORY_LIST)
+    yacht_products = published_product_query.filter(category__in=product_defs.YACHT_CATEGORY_LIST)
+    adventure_products = published_product_query.filter(category=product_defs.CATEGORY_ADVENTURE)
+
     products = []
-    count = max(space_products.count(), yacht_products.count(), adventure_products.count())
+    space_count, yacht_count, adventure_count = space_products.count(), yacht_products.count(), adventure_products.count()
+    count = max(space_count, yacht_count, adventure_count)
     for i in range(count):
-        if i < space_products.count():
+        if i < space_count:
             products.append(space_products[i])
-        if i < yacht_products.count():
+        if i < yacht_count:
             products.append(yacht_products[i])
-        if i < adventure_products.count():
+        if i < adventure_count:
             products.append(adventure_products[i])
+
     paginator = Paginator(products, defs.PER_PAGE_ITEM)
     try:
         cur_page = paginator.page(page)
-    except PageNotAnInteger:
-        cur_page = paginator.page(1)
     except EmptyPage:
         cur_page = paginator.page(paginator.num_pages)
-    if int(page) >= paginator.num_pages:
-        next_page = 0
-    else:
-        next_page = int(page) + 1
 
-    bind_product_main_image(cur_page.object_list)
     product_list = []
-    for product in cur_page.object_list:
+
+    cur_page_products = cur_page.object_list
+    bind_product_main_image(cur_page_products)
+    for product in cur_page_products:
         product_data = model_to_dict(product,
-                                     fields=['id', 'for_rental', 'for_sale', 'rental_price',
-                                             'sale_price'])
+                                     fields=['id', 'for_rental', 'for_sale', 'rental_price', 'sale_price'])
         product_data.update({
             'max_guests': product.max_guests or 0,
             'length': product.length or 0,
@@ -83,12 +122,7 @@ def home(request):
             'sale_price_display': product.get_sale_price_display(),
             'city': product.locality or '',
         })
-        liked_product_id_list = []
-        if request.user.is_authenticated:
-            liked_product_id_list = FavoriteItem.objects.filter(favorite__user=request.user).values_list(
-                'product_id', flat=True)
 
-        product_data['liked'] = product.id in liked_product_id_list
         product_data['image'] = product.main_image and product.main_image.image.url or ''
         reviews = Review.objects.filter(product=product)
         avg_score = reviews.aggregate(Avg('score'), Count('id'))
@@ -101,12 +135,14 @@ def home(request):
             })
             product_list.append(product_data)
 
-    result = {
-        'home_banner': home_banners_list,
-        'products': product_list,
-        'next_page': next_page
-    }
-    return CoastalJsonResponse(result)
+    if page >= paginator.num_pages:
+        next_page = 0
+    else:
+        next_page = page + 1
+
+    cache.set(cache_keys.CACHE_HOME_PRODUCT_LIST_KEY % page, (product_list, next_page), 5 * 60)
+
+    return product_list, next_page
 
 
 @cache_page(5 * 60)
